464884b874098326adab8ff7a2445a258d2f9192
Fainche <faicatmck@gmail.com>
2024-09-09 16:40:08 +0100

    set up a dictionary component and added a form, and functions to keep track of the state keyWord when it changes through the target value

---
+++ src/Dictionary.js
@@ -0,0 +1,37 @@
+import React, {useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    const [keyWord, setKeyWord] = useState(true);
+        
+    function search(event) {
+        event.preventDefault()
+        alert(`searching for ${keyWord} definition`);
+    }
+
+
+    function handleKeywordChange(event) {
+        setKeyWord(event.target.value);
+    }
+
+
+        return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <div className="row">
+                    <div className="col-8">
+               <input type="search"
+               placeholder="What would you like to search?" 
+               className="search-form-input"
+               autoFocus={true}
+               onChange={handleKeywordChange}
+               />
+               </div>
+               <div className="col-4">
+               <input type="submit" value="Search" className="search-form-button" /> 
+               </div>
+               </div>
+            </form>
+        </div>
+    )
+}
